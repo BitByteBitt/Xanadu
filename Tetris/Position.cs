a06c8c3885a8478b6ef34a4e132ed8a737fd26a2
Aaron Urayan <urayan.aaron@gmail.com>
2024-01-03 15:18:49 +1100

    Added Position class

---
+++ Tetris/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace Tetris
+{
+    class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int _row, int _column)
+        {
+            Row = _row;
+            Column = _column;
+        }
+    }
+}
